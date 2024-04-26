610d936f9efec9fed33ab26a14658430b17b8fd4
Ali Mousazada <mousazada.ali@gmail.com>
2024-04-25 20:05:57 -0400

    adding picture to component

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,6 +1,6 @@
 
 import styled from "./Article.module.css"
-
+import article from "./../Assets/images/javascript-736400_640.png"
 
 
 
@@ -11,7 +11,10 @@ function Article (){
     return (
 
         <div className={styled.articleWrapper}>
-            
+            <img src={article} />
+
+            <h3>Variable in Java Script</h3>
+            <p>Reading Time 5min</p>
         </div>
 
 
